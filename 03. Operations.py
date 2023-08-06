d23fbd835a16b7872d065bfcff79aced3409c68b
me_madness <p_rangel@abv.bg>
2023-08-06 21:15:34 +0200

    star with task n3

---
+++ 03. Operations.py
@@ -0,0 +1,13 @@
+number_one = int(input())
+number_two = int(input())
+operator = input()
+even_odd = ''
+result = 0
+
+
+
+
+print("{N1} {оператор} {N2} = {резултат} - {even/odd}")
+print("{N1} / {N2} = {резултат}" )
+print("{N1} % {N2} = {остатък}")
+print("Cannot divide {N1} by zero")
