5cfdc5ac7c7d86a9ffa91d321223967c98916e50
me_madness <p_rangel@abv.bg>
2023-08-06 22:24:28 +0200

    finish with task number three

--- 03. Operations.py
+++ 03. Operations.py
@@ -3,11 +3,26 @@ number_two = int(input())
 operator = input()
 even_odd = ''
 result = 0
+rest = 0
 
-
-
-
-print("{N1} {оператор} {N2} = {резултат} - {even/odd}")
-print("{N1} / {N2} = {резултат}" )
-print("{N1} % {N2} = {остатък}")
-print("Cannot divide {N1} by zero")
+if operator == "+" or operator == "-" or operator == "*":
+    if operator == "+":
+        result = number_one + number_two
+    elif operator == "-":
+        result = number_one - number_two
+    else:
+        result = number_one * number_two
+    if result % 2 == 0:
+        even_odd = 'even'
+    else:
+        even_odd = 'odd'    
+    print(f"{number_one} {operator} {number_two} = {result} - {even_odd}")
+elif operator == "/":
+    if number_two == 0:
+        print(f"Cannot divide {number_one} by zero")
+    else:
+        result = number_one / number_two
+        print(f"{number_one} / {number_two} = {result:.2f}" )
+else:  
+    rest = number_one % number_two      
+    print(f"{number_one} % {number_two} = {rest}")
